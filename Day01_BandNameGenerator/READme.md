3e2c864118da0891e9e2910d6b3e4b5b2b204d34
Musa Rizwan <musarizwan99@gmail.com>
2025-08-04 16:26:19 +0500

    Add Day 1 project: Band Name Generator

---
+++ Day01_BandNameGenerator/READme.md
@@ -0,0 +1,8 @@
+# Day 01 - Band Name Generator
+
+A simple project to generate a band name based on user input.
+
+## Skills Learned
+- Input/Output in Python
+- String concatenation
+- Variable usage
---
+++ Day01_BandNameGenerator/band_name_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to Band Name Generator")
+city_name=input("In which city did you grow up?\n")
+pet_name=input("What's your pet's name?\n")
+print("Your Band Name Could Be : " + city_name + " " + pet_name)
